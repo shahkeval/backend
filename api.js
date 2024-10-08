3a978f87397931bc78c05208df2ce89f212e51cf
shahkeval <100589596+shahkeval@users.noreply.github.com>
2024-10-08 17:13:13 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,10 @@
-console.log("deployed");
+
+app.get('/allAdmin', async (req, res) => {
+  console.log("deployed");
+  try {
+    const response = await UserModelAdmin.find({});
+    res.status(200).send(response);
+  } catch (error) {
+    console.log('Error while insert order', error);
+  }
+});
