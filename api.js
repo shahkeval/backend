3dabc4bdd32b27cccc07478a67a36f73a86511ac
shahkeval <100589596+shahkeval@users.noreply.github.com>
2024-10-08 17:16:02 +0530

    Update api.js

--- api.js
+++ api.js
@@ -8,3 +8,12 @@ app.get('/allAdmin', async (req, res) => {
     console.log('Error while insert order', error);
   }
 });
+app.post('/Admin',async (req, res) => {
+  const existingUser = await UserModelAdmin.findOne({ id: req.body.id });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User ID is not available' });
+    }
+    UserModelAdmin.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err));
+});
